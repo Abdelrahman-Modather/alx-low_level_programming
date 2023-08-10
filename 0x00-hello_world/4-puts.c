e5a8b8f857ef646c140c4bbf4cf0d1d51c6ea143
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-08-10 03:59:20 -0700

    ME ENCANTA ALX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * main - main block
-  * return: 0
-  */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
