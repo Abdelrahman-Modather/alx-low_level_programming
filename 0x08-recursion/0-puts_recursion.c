cabee642cf3160a45ebe3c128ca3082380d22000
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-08-30 05:09:53 -0700

    RECURSION

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s == 0)
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+		
