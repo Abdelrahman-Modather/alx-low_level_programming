1f8aa091101d15b12ad4e467062bfe4e976118f2
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-08-30 05:15:30 -0700

    RECURSION

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,15 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s == 0)
+
+	if (*s == '\0')
 	{
-		_putchar('\0');
-		return;
+		_putchar('\n');
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
 }
-		
