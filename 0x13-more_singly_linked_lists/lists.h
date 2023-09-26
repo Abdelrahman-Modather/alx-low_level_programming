20f7aebae1548cbc348e1ca06ed9272db7f5f557
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-09-26 11:39:10 -0700

    LINKED_LISTS2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
+#ifndef _LISTS_H_
+#define _LISTS_H_
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -23,7 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
+size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -38,6 +29,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+void free_list(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif /* LISTS_H */
+#endif /* _LISTS_H_ */
