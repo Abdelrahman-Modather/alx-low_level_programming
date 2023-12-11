382e9d49c2d367ec7c00fcf58839a156b108c66c
Abdelrahman Modather <abdelrahmanmodather32@gmail.com>
2023-12-12 00:45:03 +0200

    Dynamic Library

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strstr - prints the consecutive caracters of s1 that are in s2.
+* @haystack: pointer to char
+* @needle: pointer to char
+* Return: 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
+
